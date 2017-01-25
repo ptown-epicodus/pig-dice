5a6303ce8d57ec278598c396873f7e6c779fce62
Patrick McGreevy and Xia Amendolara <patrick@>
2017-01-25 09:27:57 -0800

    Add logic to create players

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Business logic
+var players = [];
+
 function Player(name) {
   this.name = name;
   this.score = 0;
@@ -10,7 +12,24 @@ var rollDie = function() {
 
 // Interface logic
 $(document).ready(function() {
+  $("form#new-player").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("#new-name").val();
+    var newPlayer = new Player(inputtedName);
+    players.push(newPlayer);
+
+    displayPlayers();
+  });
+
   $("button#roll").click(function() {
     $("#die-roll p").text(rollDie().toFixed());
   });
 });
+
+var displayPlayers = function() {
+  $("ol#players").empty();
+  players.forEach(function(element) {
+    $("ol#players").append("<li id='player'>" + element.name + " (" + element.score.toFixed() + ")</li>");
+  });
+};
